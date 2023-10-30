a72ea355b0b8c7f8a75fc3d70c9a56d8b9e955b1
cheolhakja <rebegin15@gmail.com>
2023-10-30 21:44:25 +0900

    add constructor, getter, toString to Guitar class

--- src/main/java/com/nefer/Guitar.java
+++ src/main/java/com/nefer/Guitar.java
@@ -6,11 +6,33 @@ public class Guitar {
     private String name;
     private GuitarStatus guitarStatus;
 
+    public Guitar(String name) { //하나의 주 생성자 활용하기
+        this(-1L, name, GuitarStatus.AVAILABLE);
+    }
+
+    public Guitar(Long id, String name, GuitarStatus guitarStatus) {
+        this.id = id;
+        this.name = name;
+        this.guitarStatus = guitarStatus;
+    }
+
     public void borrow() {
         System.out.println("대출완료");
     }
 
     public void returnGuitar() {
         System.out.println("반납완료");
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return "Guitar{" +
+                "id=" + id +
+                ", name=" + name + ", guitarStatus=" + guitarStatus +
+                '}';
     }
 }
